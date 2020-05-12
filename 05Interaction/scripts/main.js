447730c5bafad5d203707582523aeb771717b6b6
miguelfmartins18 <45131179+miguelfmartins18@users.noreply.github.com>
2020-05-12 16:05:08 +0200

    pewpew

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -1,18 +1,22 @@
-
 var rotationSpeed = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
 
-function spin(){
+function spin() {
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
+	myOtherBox.object3D.rotation.z += rotationSpeed / 2;
+	console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16);
 
 
-// myOtherBox.addEventListener('mouseenter', function(){
-// 	rotationSpeed = 0.01;
-// 	console.log('mouse enter');
-// });
+myOtherBox.addEventListener('mouseenter', function () {
+	rotationSpeed = 0.1;
+	console.log('mouse enter');
+});
+
+myOtherBox.addEventListener('mouseleave', function () {
+	rotationSpeed = 0.001;
+	console.log('mouse enter');
+});
